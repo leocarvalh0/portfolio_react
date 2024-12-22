e46b5bd2997a522f32599c1adaa2d1034166fb93
Leonardo Carvalho <0leonardocarvalho@gmail.com>
2024-12-22 15:08:46 -0300

    adicionando a section skills

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Container } from "./styles";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Sobre from "./components/Sobre/Sobre";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Container>
         <Home />
         <Sobre />
+        <Skills />
       </Container>
     </>
   );
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,122 @@
+import styled from "styled-components"
+
+const Header = styled.section`
+    display: flex;
+`
+const Titulo = styled.div`
+    font-size: 30px;
+    margin-bottom: 80px;
+    color: #ccd6f6;
+
+    &::before {
+        content: ".";
+        width: 2px;
+        margin-right: 16px;
+        background-color: #06c58c;
+    }
+`
+const Linha = styled.div`
+    background-color: rgb(68, 68, 68);
+    width: 300px;
+    height: 2px;
+    margin-top: 20px;
+    margin-left: 30px;
+`
+const SubTitulo = styled.h3`
+    font-size: 20px;
+`
+const ListaPrincipais = styled.ul`
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    margin-top: 50px;
+    margin-bottom: 80px;
+`
+const PrincipaisItem = styled.li`
+    margin-right: 16px;
+
+    img {
+        width: 60px;
+        
+        &:hover {
+            transform: translateY(-5px);
+            transition: .3s;
+        }
+    }
+`
+const ListaOutros = styled.ul`
+    margin-top: 60px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`
+const OutrosItem = styled.li`
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:hover {
+        color: #06c58c;
+        transform: translateY(-2px);
+    }
+`
+
+const Skills = () => {
+    return (
+        <section>
+        <Header>
+            <Titulo>Skills</Titulo>
+            <Linha></Linha>
+        </Header>
+        <SubTitulo>Principais conhecimentos:</SubTitulo>
+        <ListaPrincipais>
+            <PrincipaisItem>
+                <img src="images/html.png" alt=""/>
+            </PrincipaisItem>
+            <PrincipaisItem>
+                <img src="images/css.png" alt=""/>
+            </PrincipaisItem>
+            
+            <PrincipaisItem>
+                <img src="images/folder.png" alt=""/>
+            </PrincipaisItem>
+            <PrincipaisItem>
+                <img src="images/physics.png" alt=""/>
+            </PrincipaisItem>
+        </ListaPrincipais>
+        <SubTitulo>Outros conhecimentos:</SubTitulo>
+        <ListaOutros>
+            <OutrosItem>
+                <ion-icon name="caret-forward-outline"></ion-icon>
+                Bootstrap
+            </OutrosItem>
+            <OutrosItem>
+                <ion-icon name="caret-forward-outline"></ion-icon>
+                APIs
+            </OutrosItem>
+            <OutrosItem>
+                <ion-icon name="caret-forward-outline"></ion-icon>
+                TypeScript
+            </OutrosItem>
+            <OutrosItem>
+                <ion-icon name="caret-forward-outline"></ion-icon>
+                Gulp e Grunt</OutrosItem>
+            <OutrosItem>
+                <ion-icon name="caret-forward-outline"></ion-icon>
+                Git
+            </OutrosItem>
+            <OutrosItem>
+                <ion-icon name="caret-forward-outline"></ion-icon>
+                Redux
+            </OutrosItem>
+            <OutrosItem>
+                <ion-icon name="caret-forward-outline"></ion-icon>
+                Sass e Less</OutrosItem>
+            <OutrosItem>
+                <ion-icon name="caret-forward-outline"></ion-icon>
+                Jquery
+            </OutrosItem>
+        </ListaOutros>
+    </section>
+    )
+}
+
+export default Skills
