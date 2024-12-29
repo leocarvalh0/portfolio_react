bada6cdfd488a675de65477d2adf80440a4e8a6b
Leonardo Carvalho <0leonardocarvalho@gmail.com>
2024-12-29 11:29:43 -0300

    alterando o WebVitals

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,12 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    getCLS(onPerfEntry);
-    getFID(onPerfEntry);
-    getFCP(onPerfEntry);
-    getLCP(onPerfEntry);
-    getTTFB(onPerfEntry);
+    onCLS(onPerfEntry); // Medindo CLS
+    onFID(onPerfEntry); // Medindo FID
+    onFCP(onPerfEntry); // Medindo FCP
+    onLCP(onPerfEntry); // Medindo LCP
+    onTTFB(onPerfEntry); // Medindo TTFB
   }
 };
 
